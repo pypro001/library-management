dc00dc2e7ee6ac8a6fa219c5043229364238754c
vivek :) <92904472+pypro001@users.noreply.github.com>
2023-07-15 12:25:59 +0530

    Update Home.tsx

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,7 +1,3 @@
-// TODO: Fetch user data from backend and
-// greet the user with their name, then
-// show a link to their profile page which
-// would have their info and list of titles of books
 const Home = () => {
   return (
     <h1>Welcome to the Homepage!</h1>
